fcbbc0506057f654eb61fb840493e0c05dd2467b
WillianJC <willianjoelcandia@gmail.com>
2025-09-03 19:59:29 -0500

    Agregar ejemplo de como usar una api en android

---
+++ app/src/main/java/com/example/dogapiapp/DogApiService.kt
@@ -0,0 +1,8 @@
+package com.example.dogapiapp
+import com.example.dogapiapp.model.DogApiResponse
+import retrofit2.Response
+import retrofit2.http.GET
+interface DogApiService {
+    @GET("api/breeds/image/random")
+    suspend fun getRandomDogImage(): Response<DogApiResponse>
+}
